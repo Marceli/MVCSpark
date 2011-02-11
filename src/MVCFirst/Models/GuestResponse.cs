e5dbf6162c225ae1e106764fc81bf2b459855750
Marceli <marcel.piekalkiewicz@gmail.com>
2011-02-11 14:46:07 +0000

    rename to src

---
+++ src/MVCFirst/Models/GuestResponse.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MVCFirst.Models
+{
+	public class GuestResponse
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public bool? WillAttend { get; set; }
+	}
+}
